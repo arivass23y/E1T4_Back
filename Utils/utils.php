3d65e3eba9ea50a2ec6a0364c28a871ca3e4a12c
David <dlopezc24@fpsanturtzilh.eus>
2025-10-31 09:06:48 +0100

    Erabiltzailea klasea, utils eta controller adibidea.

--- Utils/utils.php
+++ Utils/utils.php
@@ -5,22 +5,45 @@ class Utils
     public static function intValidazioa($input)
     {
         if ($input !== null && !is_numeric($input)) {
-            echo "Error: no es un número";
+            http_response_code(400);
+            echo json_encode(["error" => "Datuaren formatua ez da zuzena"]);
             die();
+        }
+        else {
+            return intval($input);
         }
     }
     public static function dateValidazioa($input)
     {
         if ($input !== null && !DateTime::createFromFormat('Y-m-d', $input)) {
-            echo "Error: formato de fecha inválido";
+            http_response_code(400);
+            echo json_encode(["error" => "Datuaren formatua ez da zuzena"]);
             die();
+        }
+        else {
+            return $input;
         }
     }
     public static function stringValidazioa($input)
     {
         if ($input !== null && !is_string($input)) {
-            echo "Error: no es una cadena de texto";
+            http_response_code(400);
+            echo json_encode(["error" => "Datuaren formatua ez da zuzena"]);
             die();
+        }
+        else {
+            return $input;
+        }
+    }
+    public static function charValidazioa($input)
+    {
+        if ($input !== null && !is_string($input) && strlen($input) === 1) {
+            http_response_code(400);
+            echo json_encode(["error" => "Datuaren formatua ez da zuzena"]);
+            die();
+        }
+        else {
+            return $input;
         }
     }
 }
